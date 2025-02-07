632e06290b965389c934565f01a26f8d40f5958e
Akash-6372 <subratsahoo3883@gmail.com>
2025-02-07 19:44:13 +0530

    crud operation

--- MongooseServer/index.js
+++ MongooseServer/index.js
@@ -2,6 +2,8 @@ import  express from 'express';
 import mongoose from 'mongoose';
 const app=express()
 //Middlewares//Routes//API's
+app.use(express.json())//Json Parser
+
 app.get('/',(req,res)=>{
     res.send("hello i am sending you value from server")
 })
@@ -29,11 +31,67 @@ app.get('/',(req,res)=>{
  })
  //Define the model
  const userModel=mongoose.model("users",userSchema)//  user bound collection with userSchema
- async function getData(){
-    const data= await userModel.find()
-    console.log("users data",data)
- }
- getData()
+//  async function getData(){
+//     const data= await userModel.find()
+//     console.log("users data",data)
+//  }
+//  getData()
+///ApI's Crud Operation using mongodb Collection
+// Get All Data
+app.get('/allData', async (req,res)=>{
+    try{
+             const response=await userModel.find()
+             res.status(200).send(response)
+    }
+   catch(error){
+    res.status(500).send(error)
+   }
+
+})
+//Create document
+app.post('/add',async(req,res)=>{
+    try{
+        let data=req.body
+        //Save This  to mongodb
+        // const newUser=new userModel(data)//creating an instance of userModel
+        // const response= await newUser.save()//!Saving the instance
+        //Another Way
+        const response= await userModel.insertOne(data)
+        res.status(201).send(response)
+
+    }
+    catch(error){
+        res.status(500).send(error)
+    }
+})
+//Params:-For inserting dynamic value
+// app.post('/user/:name',(req,res)=>{
+//     console.log(req.params)
+// })
+//Update the value using params 
+app.post('/user/:name',async(req,res)=>{
+    try{
+    const {name}=req.params
+    const data=req.body
+    const response=await userModel.updateOne({name:name},{$set:{...data}})
+    res.status(200).send(response)
+    }
+    catch(error){
+        res.status(500).send(error)
+    }
+
+})
+//Delete the value from mongodb
+app.delete('/deleteuser',async(req,res)=>{
+try{
+    const data=req.query
+    const response=await userModel.deleteOne({...data});
+     res.status(200).send(response)
+}
+catch(error){
+    res.status(500).send(error)
+}
+})
 
 app.listen(7888,"localhost",()=>{
     console.log("server is running on http://localhost:7888");
